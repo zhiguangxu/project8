54d86e7df5671e87bab44b215ff6d55e42501ba8
Zhiguang Xu <xuzhiguang@gmail.com>
2014-01-20 11:44:52 -0500

    Some other tries to create line item.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,7 +27,16 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
+    
     @line_item = @cart.line_items.build(product: product)
+    # Alternatively
+    # @line_item = @cart.line_items.build
+    # @line_item.product = product
+    # or
+    # @line_item = product.line_items.build
+    # @line_item.cart = @cart
+    # or
+    # @line_item = product.line_items.build(cart: @cart)
 
     respond_to do |format|
       if @line_item.save
