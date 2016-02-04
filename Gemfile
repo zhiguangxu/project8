38bb29fc396862c4278a5f3548c56ee55c211ed6
Zhiguang Xu <xuzhiguang@gmail.com>
2016-02-03 21:24:19 -0500

    Now use Faker to generate orders. Have to modify lib/load_orders.rb

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ end
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'fog'
+gem 'faker'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- lib/load_orders.rb
+++ lib/load_orders.rb
@@ -1,3 +1,14 @@
-Order.transaction do (1..100).each do |i|
-  Order.create(name: "Customer #{i}", address: "#{i} Main Street", email: "customer-#{i}@example.com", pay_type: "Check")
-end end
+#Order.transaction do (1..100).each do |i|
+#  Order.create(name: "Customer #{i}", address: "#{i} Main Street", email: "customer-#{i}@example.com", pay_type: "Check")
+#end end
+Order.delete_all
+99.times do |n|
+	name = Faker::Name.name
+	email = Faker::Internet.safe_email(name)
+	address = Faker::Address.street_address
+	pay_type = "Check"
+	Order.create(name: name, 
+				 email: email,
+				 address: address,
+				 pay_type: pay_type)
+end
