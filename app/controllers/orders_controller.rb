8dbcec07528e0601b232289142ed0b261ea7d6fe
Zhiguang Xu <xuzhiguang@gmail.com>
2016-02-06 09:39:22 -0500

    Use will_paginate 3.1.0 now. Updated the syntax in orders controller.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,8 +6,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.paginate page: params[:page], order: 'created_at desc',
-      per_page: 10
+    @orders = Order.order('created_at desc').paginate(page: params[:page],per_page: 10)
 
     respond_to do |format|
       format.html # index.html.erb
